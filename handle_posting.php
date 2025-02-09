15326130be36ef0b84e4b563bd119361675dbe03
TristynU <turquijo@scholar.yc.edu>
2025-02-09 11:55:56 -0700

    Add files via upload

---
+++ handle_posting.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Forum Posting</title>
+</head>
+<body>
+<?php
+ini_set('display_errors', 0);
+$first_name = $_POST['first_name'];
+$last_name = $_POST['last_name'];
+$posting = $_POST['posting'];
+
+$name = $first_name . ' ' . $last_name;
+print "<div>Thank you, $name, for your posting:<p>$posting</p></div>";
+?>
+</body>
+</html>
