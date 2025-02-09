59c9ba36bc4a7af661da0f3daebc346922a5400f
TristynU <turquijo@scholar.yc.edu>
2025-02-09 16:25:50 -0700

    Add files via upload

--- handle_posting.php
+++ handle_posting.php
@@ -13,12 +13,23 @@ $last_name = $_POST['last_name'];
 $posting = nl2br($_POST['posting'], false);
 
 $name = $first_name . ' ' . $last_name;
+
+$html_post= htmlentities($_POST['posting']);
+$strip_post = strip_tags($_POST['posting']);
+
 print "<div>Thank you, $name, for your posting:<p>$posting</p></div>";
 
 $name = urlencode($name);
 $email = urlencode($_POST['email']);
 
 print "<p>Click <a href=\"thanks.php?name=$name&email=$email\">here</a> to continue.</p>";
+
+print"<div>Thank you, $name, for your posting:
+<p>Original: $posting</p>
+<p>Entity: $html_post</p>
+<p>Stripped: $strip_post</p>
+</div>";
+
 ?>
 </body>
 </html>
